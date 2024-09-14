523b956714eaec427bb79d46f78ca2492c7bd0fb
Kavi Rajesh <44529969+kavirajesh22@users.noreply.github.com>
2024-09-13 23:37:08 -0700

    Add files via upload

---
+++ helloworld.go
@@ -0,0 +1,65 @@
+package main
+
+/*
+HTTP is the protocol that is used to fetch data from web servers.
+HTTP is text sent by the client to a server to request a particular action, and the server replies.
+The following commmands are useful with curl:
+--verbose
+--trace-ascii
+
+The following APIs are implemented in this pgrm:
+- GET (Client issues a GET request to the server and receives the document asked for)
+- POST 
+- PUT * to be done*
+
+*/
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"strings"
+	"os"
+)
+
+func xget(w http.ResponseWriter, r *http.Request){
+	path:= r.URL.Path
+	parts := strings.Split(path, "/")
+
+	filename := parts[2]
+	fmt.Printf(filename)
+
+	content, err := ioutil.ReadFile(filename)
+	if err != nil{
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write(content)
+}
+
+func xpost(w http.ResponseWriter, r *http.Request){
+	path:= r.URL.Path
+	parts := strings.Split(path, "/")
+	filename := parts[2]
+	r.ParseMultipartForm(10 << 20)
+	file, handler, err := r.FormFile("file")
+
+	if err != nil{
+		return
+	}
+
+	defer file.Close()
+	outFile, err := os.Create(filename)
+	defer outFile.Close()
+
+	_, err = io.Copy(outFile, file)
+	w.WriteHeader(http.StatusCreated)
+	fmt.Printf("Uploaded the file: %s", handler.Filename)
+}
+
+
+func main(){
+	http.HandleFunc("/rkavi/", xpost)
+	fmt.Println("Server is running on port 8081...")
+	http.ListenAndServe(":8081", nil)
+}
